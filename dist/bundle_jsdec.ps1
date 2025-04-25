7d88b0137e415dce74ab4cfebc24e5b3efc8a1ae
Anton Kochkov <anton.kochkov@gmail.com>
2025-04-25 23:12:49 +0400

    Use jsdec v0.8.0

--- dist/bundle_jsdec.ps1
+++ dist/bundle_jsdec.ps1
@@ -2,7 +2,7 @@ $dist = $args[0]
 $python = Split-Path((Get-Command python.exe).Path)
 
 if (-not (Test-Path -Path 'jsdec' -PathType Container)) {
-    git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch "dev"
+    git clone https://github.com/rizinorg/jsdec.git --depth 1 --branch "v0.8.0"
 }
 cd jsdec
 $jsdecdir = (Get-Item .).FullName
